2989dab00c93d8481b4fa4ffe5369637ec982329
Jaime Esobar <jescobar@insulet.com>
2021-12-07 09:25:27 -0700

    Animations + DragGesture

---
+++ Animations/Animations/ContentView2.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView2.swift
+//  Animations
+//
+//  Created by jescobar on 12/7/21.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+    }
+}
+
+struct ContentView3: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20),
+                        value: dragAmount
+                    )
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+
+struct ContentView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView3()
+    }
+}
